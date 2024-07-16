872dae2aff1d48eb3bc9ea5ea0f7577f8508d1a2
noni <noni45100@hotmail.com>
2024-07-16 01:31:48 -0600

    Avance Ingreso

---
+++ front-end/app.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+
+
+
+</script>
+
+<template>
+  <div>
+    <header class="bg-gray-800 p-4">
+      <nav class="container mx-auto flex justify-between items-center">
+        <div class="text-white text-xl">TOAST CAFÉ</div>
+        <ul class="flex space-x-24">
+          <li>
+            <NuxtLink to="/employers_list" class="text-white hover:underline">Ingreso</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/" class="text-white hover:underline">Lista Empleados</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/" class="text-white hover:underline">Contact</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <NuxtPage />
+    <UNotifications />
+  </div>
+</template>
---
+++ front-end/pages/employers_list.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+const columns = [{
+  key: 'name',
+  label: 'Name'
+}, {
+  key: 'title',
+  label: 'Title'
+}, {
+  key: 'email',
+  label: 'Email'
+}, {
+  key: 'role',
+  label: 'Role'
+}, {
+  key: 'actions'
+}]
+
+const people = [{
+  id: 1,
+  name: 'Lindsay Walton',
+  title: 'Front-end Developer',
+  email: 'lindsay.walton@example.com',
+  role: 'Member'
+}, {
+  id: 2,
+  name: 'Courtney Henry',
+  title: 'Designer',
+  email: 'courtney.henry@example.com',
+  role: 'Admin'
+}, {
+  id: 3,
+  name: 'Tom Cook',
+  title: 'Director of Product',
+  email: 'tom.cook@example.com',
+  role: 'Member'
+}, {
+  id: 4,
+  name: 'Whitney Francis',
+  title: 'Copywriter',
+  email: 'whitney.francis@example.com',
+  role: 'Admin'
+}, {
+  id: 5,
+  name: 'Leonard Krasner',
+  title: 'Senior Designer',
+  email: 'leonard.krasner@example.com',
+  role: 'Owner'
+}, {
+  id: 6,
+  name: 'Floyd Miles',
+  title: 'Principal Designer',
+  email: 'floyd.miles@example.com',
+  role: 'Member'
+}]
+
+interface Person {
+  id: number
+  name: string
+  title: string
+  email: string
+  role: string
+}
+
+const imprimir = (row : Person) => {
+  console.log('hola')
+  console.log(row)
+}
+
+
+const selected = ref([people[1]])
+</script>
+
+<template>
+    <UTable :rows="people" :columns="columns">
+        <template #name-data="{ row }">
+        </template>
+
+        <template #actions-data="{ row }">
+            <UButton color="indigo" @click = "imprimir(row)">Marcar</UButton>
+        </template>
+    </UTable>
+</template>
+
